3209711b381db7d058a6c76966eed79b7cd29c1c
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2019-04-03 15:14:12 +0100

    css2

--- stock_exchange/settings/base.py
+++ stock_exchange/settings/base.py
@@ -209,18 +209,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../stock_exchange/static"),
-]
-print(os.path.join(BASE_DIR, 'static'))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "/static"),
+# ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'stock_exchange/staticfiles')
-# STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
 
 
 # S3 Credentials
