8b2196dd80468422a634605ad73c34e07bf15b48
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2019-04-03 15:08:25 +0100

    fix css issues

--- stock_exchange/settings/production.py
+++ stock_exchange/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'stock-exchange-env.qdtzm52k3p.us-east-1.elasticbeanstalk.com','stormy-lake-21329.herokuapp.com'
+    'stock-exchange-env.qdtzm52k3p.us-east-1.elasticbeanstalk.com','stormy-lake-21329.herokuapp.com', '52.22.196.40'
 ]
 
-ADMINS = (('@memunat', 'taiwo.sokunbi@andela.com'), )
+ADMINS = (('@memunat', 'thepanache27@gmail.com'), )
