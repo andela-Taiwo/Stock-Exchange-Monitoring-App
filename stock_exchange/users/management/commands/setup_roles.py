16d80a54efcbac18121615ed8afb546dd91b3194
SOKUNBI TAIWO <sokunbitaiwo82@gmail.com>
2019-04-06 16:55:29 +0100

    Staging (#11)
    * add permissions
    add role model
    add role endpoints
    add tests for role enpoints
    
    * update settingd
    
    * add static file
    
    * fixing css issues
    
    * debug static
    
    * fixing csss
    
    * fixing statics
    
    * fixing whitenoise
    
    * fixing whitenoise issue
    
    * test
    
    * test2
    
    * fix login url'
    
    * update readme
    
    * Fix css issues
    add aws config
    
    * fix css issues
    
    * css
    
    * css2
    
    * add roles script
    update allowed hosts
    update aws django config

---
+++ stock_exchange/users/management/commands/setup_roles.py
@@ -0,0 +1,59 @@
+from django.core.management.base import BaseCommand, CommandError
+from  users.models import (User, Role, Permission)
+from django.conf import settings
+from django.contrib.sites.models import Site
+
+class Command(BaseCommand):
+
+    def add_user_roles(self, roles_and_permissions):
+        ''' Creates multiple roles each with permissions passed as list of pair tuples (resource, action) '''
+        ''' Generally just use `add_user_permissions`. '''
+
+        assert isinstance(roles_and_permissions, list) or isinstance(roles_and_permissions, tuple)
+
+        for role_name, role_permissions in roles_and_permissions:
+            role = Role.objects.create(label=role_name)
+            for resource, action in role_permissions:
+                permission, _ = Permission.objects.get_or_create(resource=resource, action=action)
+                role.permissions.add(permission)
+ 
+
+    def handle(self, *args, **options):
+        
+        admin_permissions = [
+            ('roles', 'list'),
+            ('roles', 'create'),
+            ('roles', 'update'),
+            ('roles', 'retrieve'),
+            ('user.role', 'list'),
+            ('user.role', 'update'),
+            ('user', 'retrieve'),
+            ('user', 'retrieve-any'),
+            ('stocks', 'list'),
+            ('stocks', 'upload'),
+            ('stocks', 'filter'),
+        ]
+        user_permissions = [
+            ('stocks', 'list'),
+            ('stocks', 'filter'),
+        ]
+        roles_user = (
+            ('Admin', admin_permissions),
+            ('User', user_permissions)
+        )
+        
+        self.add_user_roles(roles_user)
+        admin = User.objects.filter(email=settings.ADMIN_USER_EMAIL)
+        if admin.exists():
+            admin = admin.first()
+            admin.is_admin = True
+            admin.is_superuser = False
+            admin.save()
+            admin.profile.roles.add(Role.objects.get(label='Admin'))
+        site = Site.objects.get(domain='example.com')
+        site.domain='stock-exchange-env.qdtzm52k3p.us-east-1.elasticbeanstalk.com'
+        site.name='elasticbeanstalk.com'
+        site.save()
+        self.stdout.write('Successfully created created roles')
+
+        
