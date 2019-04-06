23b1be303bfc4d3507d88fc690c8a5603b40187b
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2019-04-06 15:33:29 +0100

    add roles script update allowed hosts update aws django config

--- stock_exchange/settings/production.py
+++ stock_exchange/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'stock-exchange-env.qdtzm52k3p.us-east-1.elasticbeanstalk.com','stormy-lake-21329.herokuapp.com', '52.22.196.40'
+    'stock-exchange-env.qdtzm52k3p.us-east-1.elasticbeanstalk.com','stormy-lake-21329.herokuapp.com'
 ]
 
 ADMINS = (('@memunat', 'thepanache27@gmail.com'), )
