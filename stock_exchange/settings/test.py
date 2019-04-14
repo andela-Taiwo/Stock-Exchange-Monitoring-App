4191d6510b196554a0e09163bdf24b9e57e99453
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2019-04-14 12:46:35 +0100

    add es config to circleci

--- stock_exchange/settings/test.py
+++ stock_exchange/settings/test.py
@@ -2,11 +2,6 @@ import os
 import sys
 from settings.base import *
 
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase'
-    }
 
 DATABASES = {
     'default': {
@@ -14,8 +9,19 @@ DATABASES = {
         'NAME':  ':memory:',
     }
 }
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase'
+    }
+
 ELASTICSEARCH_INDEX_NAMES = {
     'search_indexes.documents.stocks': 'test_stock',
     # 'search_indexes.documents.publisher': 'publisher',
+}
+es_test_cluster_config = {
+    "port": 9250, "number_of_nodes": 1,
+    "network_host": '_local_',
+    "cluster_name": 'group-project-test-cluster' 
 }
 SECRET_KEY = os.environ.get('SECRET_KEY')
